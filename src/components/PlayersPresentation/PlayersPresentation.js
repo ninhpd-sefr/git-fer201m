755d06faebade552b856cbba5c329198b9bf39df
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-05-30 16:39:44 +0700

    exercise 10

--- src/components/PlayersPresentation/PlayersPresentation.js
+++ src/components/PlayersPresentation/PlayersPresentation.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlayersPresentation.css";
 
 function PlayersPresentation({ PlayerList }) {
-  console.log(PlayerList);
+  const [player, setPLayer] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handlePopup = (player) => {
+    setIsOpen(true);
+    setPLayer(player);
+  };
 
   return (
     <div className="container">
@@ -13,11 +19,26 @@ function PlayersPresentation({ PlayerList }) {
             <h3>{player.name}</h3>
             <p className="title">{player.club}</p>
             <p>
-              <button>Detail</button>
+              <button onClick={() => handlePopup(player)}>Detail</button>
             </p>
           </div>
         </div>
       ))}
+
+      {isOpen ? (
+        <div className="overlay">
+          <div className="popup">
+            <img src={player.img} />
+            <h2>{player.name}</h2>
+            <a className="close" href="#" onClick={() => setIsOpen(false)}>
+              X
+            </a>
+            <div className="content">{player.info}</div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
