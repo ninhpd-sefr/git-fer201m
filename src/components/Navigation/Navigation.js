e939d0d810e3aefc2cb20b0d3a773d995e0997b1
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-06-05 20:25:04 +0700

    exercise 11

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,57 @@
-import React from "react";
-import './Navigation.css';
+import React, { useContext } from "react";
+import "./Navigation.css";
+import { ThemeContext } from "../ThemeContext";
 
 function Navigation() {
+  const { theme, toggle, dark } = useContext(ThemeContext);
+  console.log(theme);
+
   return (
     <div>
-      <nav>
-        <ul className="navBar">
-          <li><a className="active" href="#home">Home</a></li>
-          <li><a href="#news">News</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
+      <nav
+        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <ul>
+          <li>
+            <a style={{ color: theme.color }} className="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a style={{ color: theme.color }} href="#news">
+              News
+            </a>
+          </li>
+          <li>
+            <a style={{ color: theme.color }} href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a style={{ color: theme.color }} href="#contact">
+              Contact
+            </a>
+          </li>
         </ul>
+
+        <div style={{ position: "relative" }}>
+          <a
+            className="switch-mode"
+            href="#"
+            onClick={toggle}
+            style={{
+              backgroundColor: theme.backgroundColor,
+              color: theme.color,
+              outline: "none",
+            }}
+            data-testid="toggle-theme-btn"
+          >
+            Switch Nav to {!dark ? "Dark" : "Light"} mode
+          </a>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
